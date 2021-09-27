a196853e3ad76eb5f76afaa0cde14ff70213b0d6
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-27 00:43:15 -0500

    working

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 function GetInfo(){
     const newName = document.getElementById("cityInput");
     const cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--"
+    cityName.innerHTML = "--"+newName.value+"--";
 
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='newName.value+'&appid=593c1929abed320b8081e98e1d74384f")
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=593c1929abed320b8081e98e1d74384f')
 .then(response => response.json())
 .then(data => {
 
     // TO GET MIN AND MAX TEMP
     for(i = 0; i<5; i++){
-        document.getElementById("day" +(i+1)+"Min").innerHTML ="Min:" + Number(data.list[i].main.temp_min -305.83).toFixed(1)+"°";
+        document.getElementById("day" +(i+1)+"Min").innerHTML ="Min: " + Number(data.list[i].main.temp_min -305.83).toFixed(1)+"°";
     }
 
     for(i = 0; i<5; i++){
-        document.getElementById("day" +(i+1)+"Max").innerHTML ="Max:" + Number(data.list[i].main.temp_max -305.83).toFixed(2)+"°";
+        document.getElementById("day" +(i+1)+"Max").innerHTML ="Max: " + Number(data.list[i].main.temp_max -305.83).toFixed(2)+"°";
     }
 
     // TO GET ICONS
 
     for(i = 0; i<5; i++){
-        document.getElementById("img" +(i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png"; 
+        document.getElementById("img" +(i+1)).src ="http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png"; 
         // console.log(data) 
     }
 })
